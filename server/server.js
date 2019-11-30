ce3154336c959e321bef680a13de3d0a63dfb2fe
Jorge Lopez <pruebas@instancia2.es>
2019-11-30 08:09:05 +0100

    Primer commit del proyecto

---
+++ server/server.js
@@ -0,0 +1,31 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// Inicializar variables
+var app = express();
+
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
+
+// Conexión a la base de datos
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb://admin:admin@instancia2.com:27017/geritronic_dev1', (err, res) => {
+
+    if (err) throw err;
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
+});
