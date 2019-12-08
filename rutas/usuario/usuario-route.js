b5b414e104dbe186542d790ffa8e974f7d77e257
Jorge Lopez <pruebas@instancia2.es>
2019-12-08 01:54:31 +0100

    Implementamos la funcionalidad de comprobar si la cuenta de usuario/cliente esta validada y activa antes de dar de alta los datos. Implementamos los enpoints para añadir nuevos tlf de contactos y nuevos títulos a los datos del usuario.

--- rutas/usuario/usuario-route.js
+++ rutas/usuario/usuario-route.js
@@ -4,6 +4,7 @@ var jwt = require('jsonwebtoken');
 var { verificaToken, verificaAdminRole } = require('../../middlewares/autenticacion');
 var app = express();
 var Usuario = require('../../models/usuario');
+var RegistroUsuarios = require('../../models/new-user');
 
 // ========================================================
 // Obtener todos los usuarios filtrado por datos personales
@@ -38,46 +39,78 @@ app.post('/', (req, res) => {
 
     var body = req.body;
 
-    //Objeto datos de acceso del usuario para poder encriptar la pass del usuario
-    var usuarioLogin = {
-        email: String,
-        pass: String
-    };
+    var regex = new RegExp(body.datos_acceso.email, 'i');
 
-    usuarioLogin.email = body.datos_acceso.email;
-    usuarioLogin.pass = bcrypt.hashSync(body.datos_acceso.pass, 10);
+    // Primero comprobamos que el usuario tiene validado el email
+    buscarUsuarioByEmail(body.datos_acceso.email, regex)
+        .then(usuario => {
 
-    //Obtenemos la fecha de insercion en formato IsoDate
-    var fecha = new Date();
-    var isoDate = fecha.toISOString();
+            // Comprobamos que hemos encontrado el usuario en usuarios registrados
+            if (usuario.length <= 0) {
+                res.status(401).json({
+                    ok: false,
+                    mensaje: 'No se ha encontrado ningun usuario registrado con email: ' + body.datos_acceso.email
+                });
+            } else {
+                // Si el usuario está en el registro de usuario, comprobamos si el email está validado y activo
+                if (!usuario.valid || !usuario.active) {
+                    console.log("Usuario inactivo o no validado");
+                    res.status(401).json({
+                        ok: false,
+                        mensaje: 'Usuario inactivo o no validado ' + body.datos_acceso.email
+                    });
 
-    var usuario = new Usuario({
-        datos_personales: body.datos_personales,
-        ubicacion: body.ubicacion,
-        datos_acceso: usuarioLogin,
-        desc: body.desc,
-        datos_laborales: body.datos_laborales,
-        role: body.role,
-        dateAdd: isoDate,
-        active: true
-    });
+                } else {
+                    console.log("Usuario valido y activo");
 
-    usuario.save((err, usuarioGuardado) => {
+                    //Objeto datos de acceso del usuario para poder encriptar la pass del usuario
+                    var usuarioLogin = {
+                        email: String,
+                        pass: String
+                    };
 
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'Error al crear usuario',
-                errors: err.message
-            });
-        }
+                    usuarioLogin.email = body.datos_acceso.email;
+                    usuarioLogin.pass = bcrypt.hashSync(body.datos_acceso.pass, 10);
 
-        res.status(201).json({
-            ok: true,
-            mensaje: 'Usuario creado correctamente',
-            usuario: usuarioGuardado.datos_personales
+                    //Obtenemos la fecha de insercion en formato IsoDate
+                    var fecha = new Date();
+                    var isoDate = fecha.toISOString();
+
+                    var usuario = new Usuario({
+                        datos_personales: body.datos_personales,
+                        ubicacion: body.ubicacion,
+                        datos_acceso: usuarioLogin,
+                        desc: body.desc,
+                        datos_laborales: body.datos_laborales,
+                        role: body.role,
+                        dateAdd: isoDate,
+                        active: true
+                    });
+
+                    usuario.save((err, usuarioGuardado) => {
+
+                        if (err) {
+                            return res.status(400).json({
+                                ok: false,
+                                mensaje: 'Error al crear usuario',
+                                errors: err.message
+                            });
+                        }
+
+                        res.status(201).json({
+                            ok: true,
+                            mensaje: 'Usuario creado correctamente',
+                            usuario: usuarioGuardado.datos_personales
+                        });
+                    });
+                }
+            }
+
+        })
+        .catch(err => {
+            console.log(err);
         });
-    });
+
 });
 
 // ==========================================
@@ -143,6 +176,72 @@ app.put('/', verificaToken, (req, res) => {
     });
 });
 
+// ====================================================================
+// Actualizamos los datos de usuario añadiendo un nuevo tlf de contacto
+// ====================================================================
+app.put('/contact', (req, res) => {
+
+    var body = req.body;
+    var id = body.id;
+
+    console.log("Dentro de contact: " + id);
+
+    Usuario.findByIdAndUpdate(id, { $push: { "datos_personales.contacto": { tlf: body.tlf } } }, (err, contactoGuardado) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al crear nuevo contacto',
+                errors: err
+            });
+        } else {
+            return res.status(200).json({
+                ok: true,
+                menaaje: 'Nuevo contacto creado correctamente',
+                titulo: contactoGuardado
+            });
+        }
+    });
+
+});
+
+// ================================================================
+// Actualizamos los datos de usuario añadiendo una nueva titulacion
+// ================================================================
+app.put('/titulo', (req, res) => {
+
+    var body = req.body;
+    var id = body.id;
+
+    console.log("Dentro de titulo: " + id);
+
+    Usuario.findByIdAndUpdate(id, { $push: { "datos_laborales.titulos": { nombre: body.nombre, fecha: body.fecha } } }, (err, titulo) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al crear nuevo titulo',
+                errors: err
+            });
+        }
+
+        if (!titulo) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id ' + id + ' no existe',
+                errors: { message: 'No existe un usuario con ese ID' }
+            });
+        } else {
+            return res.status(200).json({
+                ok: true,
+                menaaje: 'Nuevo titulo creado correctamente',
+                titulo: titulo
+            });
+        }
+
+    });
+
+});
+
 
 // ============================================
 //   Borrar un usuario por el id, solo podrá hacerlo
@@ -192,5 +291,25 @@ app.delete('/', [verificaToken, verificaAdminRole], (req, res) => {
         });
     });
 });
+
+
+// =================================
+// Buscamos usuario por el email
+// =================================
+function buscarUsuarioByEmail(email, regex) {
+
+    return new Promise((resolve, reject) => {
+
+        RegistroUsuarios.findOne({ email: regex })
+            .exec((err, usuario) => {
+
+                if (err) {
+                    reject('Error al buscar el usuario con email: ' + email + '', err);
+                } else {
+                    resolve(usuario);
+                }
+            });
+    });
+}
 
 module.exports = app;
